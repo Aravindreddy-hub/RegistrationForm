286a1084c050b6921127f6b6d21d43563634e7a3
Aravindreddy-hub <komandlaaravindreddy61@gmail.com>
2025-09-13 19:52:08 -0500

    Login.js

--- frontend/frontend/src/Login.js
+++ frontend/frontend/src/Login.js
@@ -18,7 +18,7 @@ function Login() {
     }
     
     try {
-      const res = await axios.post("http://localhost:5000/login", { email, password });
+      const res = await axios.post("https://registrationform-ls2p.onrender.com", { email, password });
       localStorage.setItem("token", res.data.token);
       alert("Login successful");
       navigate("/home");
